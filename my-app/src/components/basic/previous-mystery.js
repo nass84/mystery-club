422e8da94238e1e39dc558d524d39d6ab79939ff
Becki Floyd <beckifloyd@gmail.com>
2021-10-13 23:56:50 +0100

    Added previous mystery

--- my-app/src/components/basic/previous-mystery.js
+++ my-app/src/components/basic/previous-mystery.js
@@ -1,25 +1,30 @@
-import battingCage from '../../images/mystery-pictures/battingcages.jpg'
-
+import previousmystery from '../../images/mystery-pictures/planet.jpg'
+import previous from "../../images/mystery-pictures/previous.png"
 
 const Previous = () => {
-  const name = "Gannon";
-  const link = "https://www.thefloodgate.co.uk/"
-  const location ="Bleachers Baseball - Batting Cage"
+  const name = "Max";
+  const link = "https://www.facebook.com/WonderDomePlanetariumUK/"
+  const location ="Inflatable Planetarium"
   return (
     <div className="previous">
       <h1 className="picture-title">Previous Mystery</h1>
       <div className="previous-mystery">
       <h2>What was the Mystery?</h2> 
-        <p className="quote"> {location} </p>
-         <p> Location: <a className="previous-link" href={link}> Flood Gate - Digbeth </a></p>
+        <p className="mysteryname"> {location} </p>
+         <p> <a className="previous-link" target="_blank" rel="noreferrer" href={link}> Click Here to find out more </a></p>
           <p> Chosen by: {name} </p>
         <img
           className="previous-picture"
-          src={battingCage}
+          src={previousmystery}
           alt={location}
         />
                
       </div>
+      <img
+          className="previous-picture"
+          src={previous}
+          alt={location}
+        />
     </div>
   );
 };
